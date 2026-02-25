7f1914db1cbd970f6094063d2e948d1ff8d86298
Anray <asheng27@nobles.edu>
2026-02-24 19:54:37 -0500

    Add another print line lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Anray Sheng" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I love cooking!")
 
 if __name__ == "__main__":
     main()
